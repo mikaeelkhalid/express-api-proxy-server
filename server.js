edf93e1d11c9357e1d0b2034ddad9f965cf3334e
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-21 14:26:29 +0500

    adding rate limiter

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const { application } = require('express');
 require('dotenv').config();
+const rateLimit = require('express-rate-limit');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// rate limiter
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+});
+app.use(limiter);
+app.set('trust proxy', 1); // trust first proxy
 
 // routes
 app.use('/api', require('./routes'));
