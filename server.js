149a364de2d868fb364a2866eff9956b7aeb6fd5
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-10-21 13:49:33 +0500

    setting routes

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const { application } = require('express');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// routes
+app.use('/api', require('./routes'));
 
 // enable cors
 app.use(cors());
